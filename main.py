66f0c05e19a718399fdd65cca175c9025f194134
Nikita <n.rettyov@gmail.com>
2020-08-11 00:55:50 +0700

    first read RAM

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
 import memoryTools as memTs
+from sys import byteorder
+
+import win32ui
+import win32process
+from ctypes import wintypes, windll, c_char_p, c_ulong, byref,c_ubyte,create_string_buffer,c_size_t
+
 
 if __name__ == '__main__':
     a = memTs.MemoryTools()
     print(a.__doc__)
+
+    ### Initializing functions and permissions ###
+    OpenProcess = windll.kernel32.OpenProcess
+    ReadProcessMemory = windll.kernel32.ReadProcessMemory  # Method 1
+
+    PROCESS_ALL_ACCESS = 0x1F0FFF
+    PROCESS_QUERY_INFORMATION = 0x0400
+    PROCESS_VM_OPERATION = 0x0008
+    PROCESS_VM_READ = 0x0010
+    PROCESS_VM_WRITE = 0x0020
+    ### End of Initializing session
+
+    ### Getting process handle ###
+    HWND = win32ui.FindWindow(None, 'ShellShock Live').GetSafeHwnd()
+    PID = win32process.GetWindowThreadProcessId(HWND)[1]
+    processHandle = OpenProcess(PROCESS_ALL_ACCESS, False, PID)  # Why is it zero
+    ### End of Getting process handle ###
+    print('HWND is', HWND, '\nPID is', PID, '\nprocessHandle is', processHandle, '\n')
+    ### Reading value of a Memory Address ###
+
+    ADDRESS = 0x39C9694C
+    buffer = c_char_p(b"data")
+    buffer_size = len(buffer.value)
+    bytes_read = c_ulong(0)
+    bytes_written = c_ulong(0)
+
+    isRead = ReadProcessMemory(processHandle, ADDRESS, buffer, buffer_size, byref(bytes_read))
+    value = int.from_bytes(buffer.value, byteorder=byteorder)
+    windll.kernel32.CloseHandle(processHandle)
+    print('Memory Value =', value, buffer.value)
--- memoryTools.py
+++ memoryTools.py
@@ -1,3 +1,17 @@
+'''
+int GetProcessIdByWindowName(LPCWSTR className, LPCWSTR windowName);
+int GetProcessIdByProcessName(LPCWSTR processName);
+int WriteMem(HANDLE hProcess, LPVOID address, LPVOID source, SIZE_T writeAmount);
+LPVOID ReadMem(HANDLE hProcess, LPVOID address, SIZE_T readAmount);
+LPVOID AllocMem(HANDLE hProcess, LPVOID startAddress, SIZE_T allocationAmount);
+int FreeMem(HANDLE hProcess, LPVOID address, SIZE_T amount);
+LPVOID ScanSignature(HANDLE hProcess, ULONG_PTR startAddress, SIZE_T scanSize, PBYTE pattern, std::wstring& mask);
+bool CheckSignature(PBYTE source, PBYTE pattern, std::wstring& mask);
+void ShowErrorMessage(HWND hwnd, LPCWSTR errorMessage);
+bool isTargetx64Process(HANDLE hProcess);
+DWORD_PTR GetProcessBaseAddress(HANDLE hProcess);
+DWORD_PTR GetModuleBaseAddress(HANDLE hProcess, LPCWSTR lpszModuleName);
+'''
 class MemoryTools:
     '''This class provides all the functions with RAM'''
     def __init__(self):
