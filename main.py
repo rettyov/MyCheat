b2149c480a33da696489cd6e8cc10660cb091aba
Nikita <n.rettyov@gmail.com>
2020-08-10 17:28:52 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import memoryTools as memTs
+
+if __name__ == '__main__':
+    a = memTs.MemoryTools()
+    print(a.__doc__)
---
+++ memoryTools.py
@@ -0,0 +1,4 @@
+class MemoryTools:
+    '''This class provides all the functions with RAM'''
+    def __init__(self):
+        pass
